27a477c87ae95ab5e226a0d81e593daf51743690
smileymitha <sushmithasmiley27@gmail.com>
2017-02-21 17:09:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+
+//change the text of the main text div
+var element=document.getelementbyid('main-text');
+
+element.innerhtml="new value";
+
+//move the image
+var element=document.getelementbyid('img');
+img.onclick=function(){
+    img.style.marginleft="100px";
+};
